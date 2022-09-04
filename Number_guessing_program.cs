740b887c768b7a1679fcab70543e5abae28ae1cd
Paul Onyebuchi <70877999+ecoderP@users.noreply.github.com>
2022-09-04 21:35:56 +0100

    Update Number_guessing_program.cs

--- Number_guessing_program.cs
+++ Number_guessing_program.cs
@@ -1,1 +1,11 @@
+using System;
 
+public class NumberGuessProgram
+{
+    public static void Main(string[] args)
+    {
+        Random rand = new Random();
+        int randNum = rand.Next(1,51);
+        Console.WriteLine(randNum);
+    }
+}
